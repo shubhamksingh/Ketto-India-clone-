739a1d44fd28c47c896a3c28acf194b87ed8dc73
shubham <shubh0795@gmail.com>
2022-08-25 01:32:25 +0530

    a

--- src/index.js
+++ src/index.js
@@ -3,12 +3,21 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {BrowserRouter} from 'react-router-dom';
+import { AppContextProvier } from './Context/Context';
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
+  <AppContextProvier>
+      <BrowserRouter>
+    <React.StrictMode>
     <App />
   </React.StrictMode>
+  </BrowserRouter>
+  </AppContextProvier>
+
+
 );
 
 // If you want to start measuring performance in your app, pass a function
