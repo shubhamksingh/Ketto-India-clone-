2253683ea4596d3c1cd96f09dc36a57df5c90f47
shubham <shubh0795@gmail.com>
2022-08-26 23:49:20 +0530

    ad

--- src/Routes/Home.jsx
+++ src/Routes/Home.jsx
@@ -1,7 +1,14 @@
+import styles from './Home.module.css'
 export const Home = () => {
   return (
     <div>
-       <div></div>
+      <div className={styles.banner}>
+        <div>
+          <h2>Need Funds to Pay For a Medical Emergency or Social Cause?</h2>
+          <div></div>
+          <div></div>
+      </div>
+      </div>
     </div>
   )
 };
