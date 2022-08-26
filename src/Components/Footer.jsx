1021626c968442d336ad6ebbc14bf954bf6630d6
shubham <shubh0795@gmail.com>
2022-08-27 02:53:49 +0530

    ad

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,5 +1,31 @@
+import { Image } from "@chakra-ui/react";
+import styles from "./Footer.module.css";
 export const Footer = () => {
-    return (
+  return (
+    <div className={styles.footer}>
+      <div>
+       <div>
         <div></div>
-    )
-}
+        <div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+        </div>
+       </div>
+       
+        <div style={{ display: "flex", justifyContent: "flex-end" }}>
+          <Image
+            src="https://ketto.gumlet.io/assets/images/homepage/footer-secured-card.png?w=457&dpr=1.1"
+            width="400px"
+            height="30px"
+          />
+        </div>
+        <div style={{borderTop: '1px solid grey ' , paddingTop: '5px', marginTop: '10px', fontSize:'10px'}}>
+          Copyright © 2022 Ketto Online Ventures Pvt Ltd. All Rights Reserved.
+          Terms of Use | Privacy Policy | AML Policy | Use of cookies{" "}
+        </div>
+      </div>
+    </div>
+  );
+};
